8006014cfae0d435745ff9ccce45c95cfe9c1d89
Eduardo Sánchez Villafán <basov_chem_eduardo_sanchez@hotmail.com>
2020-07-19 22:14:38 -0500

    DB file cleanup

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,4 @@
 -- The line below is commented out to prevent data loss, uncomment if necessary
-
 DROP DATABASE IF EXISTS roadmap_planner;
 CREATE DATABASE roadmap_planner;
 USE roadmap_planner;
-
-SELECT * from users;
-
-SELECT * from projects;
-INSERT INTO tasks
-VALUE
-
-SELECT * from tasks;
